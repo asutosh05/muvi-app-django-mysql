18cb7e456db0b31e049bb64d6be7272178b537cf
Asutosh <asutosh407@gmail.com>
2018-08-05 16:03:18 +0000

    day 22

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import CreateView,DetailView
 from django.contrib.auth import logout
-# Create your views here.
+
 from .forms import RegisterForm
 
+# Regsiter view your views here.
 class RegisterView(CreateView):
     form_class=RegisterForm
     template_name='registration/register.html'
     success_url='/login/'
 
+#Default home view
 def home_view(request):
     return render(request,"base.html")
 
 
-
+#Logout view
 def logout_view(request):
     logout(request)
     return render(request,"registration/login.html")
